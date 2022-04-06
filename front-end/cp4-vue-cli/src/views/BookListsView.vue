958335d69f9d185ff686267d0cf1b1f52a89c9d6
math919191 <math919191@gmail.com>
2022-04-06 10:12:06 -0600

    cont. redoing pages

--- front-end/cp4-vue-cli/src/views/BookListsView.vue
+++ front-end/cp4-vue-cli/src/views/BookListsView.vue
@@ -1,8 +1,35 @@
 <template>
     <div>
-    <button  @click="getAllBooksFromList('favorites')">Favorites List</button>
-    <button  @click="getAllBooksFromList('completed')">Completed List</button>
-    <button  @click="getAllBooksFromList('booksToRead')">Reading List</button>
+    <button  @click="switchComponent('favorites')">Favorites List</button>
+    <button  @click="switchComponent('completed')">Completed List</button>
+    <button  @click="switchComponent('booksToRead')">Reading List</button>
 
     </div>
 </template>
+
+<script>
+    import CompletedList from "../components/CompletedList.vue"
+    import FavoritesList from "../components/FavoritesList.vue"
+    import ReadingList from "../components/ReadingList.vue"
+    
+    export default {
+        name: "BooksLists", 
+        data() {
+            return {
+                whichComponent: "",    
+            }
+        },
+        components: {
+            CompletedList,
+            FavoritesList,
+            ReadingList,
+        },
+        methods: {
+            switchComponent(){
+
+            }
+        }
+    }
+
+</script>
+
